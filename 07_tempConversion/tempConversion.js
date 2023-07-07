9a3ed7ef7b8a5783069171ffdd582f20b86b3de8
johnyousef <johnthecoderr@gmail.com>
2023-07-07 22:36:44 +0300

    finished tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+// °F = (9/5) °C+32
+const convertToCelsius = function(f) {
+  return Math.round(10 * ((f - 32) / (9/5))) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  return  Math.round(10 * (c * (9/5) + 32)) / 10
 };
 
 // Do not edit below this line

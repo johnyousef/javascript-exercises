57c3ce271bf88a74bc677241e41f1c2074dc6a01
johnyousef <johnthecoderr@gmail.com>
2023-07-07 21:23:52 +0300

    finished removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+    let new_array = []
 
+    for (let i = 0; i < array.length; i++){ 
+        if(!args.includes(array[i])){
+            new_array.push(array[i]);
+        }
+    }
+    return new_array;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;

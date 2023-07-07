63aa54103d20fc55233818d20f31e906f3acbfa0
johnyousef <johnthecoderr@gmail.com>
2023-07-07 20:03:29 +0300

    finished reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+function join_array(a) {
+    let str = '';
+    for (let i = 0; i < a.length; i++) {
+        str += a[i];
+    }
+    return str;
+}
 
+const reverseString = function(str) {  
+    let str_list = str.split('');
+    let new_str_list = [];
+    for (let i = 0; i < str_list.length; i++){
+       new_str_list[i] = str_list[str_list.length - (i + 1)];  
+    }
+    return join_array(new_str_list);
 };
 
 // Do not edit below this line

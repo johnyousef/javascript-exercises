a82ce5991485f4778fe535563f1a002cd3a4e144
johnyousef <johnthecoderr@gmail.com>
2023-07-07 19:23:32 +0300

    finished the second exercise '02_repeatString'

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    
+    if (num < 0) {
+        return "ERROR";
+    }
+    
+    let answer = '';
+    for(let i = 0; i < num; i++) {
+        answer = answer.concat(str);
+    }
+    return answer;
 };
 
 // Do not edit below this line
